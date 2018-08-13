229105ed0bead4ceadb3a26cf29f6921817aab86
shishito.megane my_ubuntu <shishito.megane3060.3@gmail.com>
2018-08-14 00:43:48 +0900

    HomeActivity <- display welcome message

--- app/src/main/java/shishito_megane/github/io/dest_bbs_client_android_app/HomeActivity.java
+++ app/src/main/java/shishito_megane/github/io/dest_bbs_client_android_app/HomeActivity.java
@@ -3,14 +3,23 @@ package shishito_megane.github.io.dest_bbs_client_android_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        String lab_name = getString(R.string.lab_name);
+        String welcome_msg = getString(R.string.welcome_msg, lab_name);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         View decor = this.getWindow().getDecorView();
+
+        TextView textView = findViewById(R.id.textView_welcome_msg);
+        textView.setText(welcome_msg);
+
         decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
