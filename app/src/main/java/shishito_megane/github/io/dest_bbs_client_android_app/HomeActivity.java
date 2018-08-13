7faddd0f3da4f0a78ef7ed62468f270d6f0a627e
shishito.megane my_ubuntu <shishito.megane3060.3@gmail.com>
2018-08-13 23:54:56 +0900

    HomeActivity <- Set full screen

--- app/src/main/java/shishito_megane/github/io/dest_bbs_client_android_app/HomeActivity.java
+++ app/src/main/java/shishito_megane/github/io/dest_bbs_client_android_app/HomeActivity.java
@@ -2,6 +2,7 @@ package shishito_megane.github.io.dest_bbs_client_android_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        View decor = this.getWindow().getDecorView();
+        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 }
