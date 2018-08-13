a7be2768222193b060055cea9d1775a5fe3f21e3
shishito.megane my_ubuntu <shishito.megane3060.3@gmail.com>
2018-08-13 16:46:54 +0900

    新規にアプリを作成

---
+++ app/src/main/java/shishito_megane/github/io/dest_bbs_client_android_app/HomeActivity.java
@@ -0,0 +1,13 @@
+package shishito_megane.github.io.dest_bbs_client_android_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
