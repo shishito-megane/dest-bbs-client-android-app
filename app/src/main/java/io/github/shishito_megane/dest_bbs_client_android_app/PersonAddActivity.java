4729bcea18ab3b5cef66b825acc08f8082cd982c
shishito.megane my_ubuntu <shishito.megane3060.3@gmail.com>
2018-08-20 17:29:16 +0900

    Add get value function <- PersonAddActivity

--- app/src/main/java/io/github/shishito_megane/dest_bbs_client_android_app/PersonAddActivity.java
+++ app/src/main/java/io/github/shishito_megane/dest_bbs_client_android_app/PersonAddActivity.java
@@ -1,9 +1,14 @@
 package io.github.shishito_megane.dest_bbs_client_android_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class PersonAddActivity extends AppCompatActivity {
 
@@ -19,12 +24,36 @@ public class PersonAddActivity extends AppCompatActivity {
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-//        int id = item.getItemId();
-//        if (id == R.id.home) {
-//            Intent intent = new Intent(this, NextActivity.class);
-//            intent.putExtra("hoge", "hoge");
-//            startActivity(intent);
-//        }
+
+        // get person info
+        EditText editPersonName = (EditText)findViewById(R.id.editTextPersonName);
+        String personName = editPersonName.getText().toString();
+        EditText editPersonAddress = (EditText)findViewById(R.id.editTextPersonAddress);
+        String personAddress = editPersonAddress.getText().toString();
+        EditText editTextPersonDetail = (EditText)findViewById(R.id.editTextPersonDetail);
+        String personDetail = editTextPersonDetail.getText().toString();
+        EditText editTextCalenderId = (EditText)findViewById(R.id.editTextCalenderId);
+        String personCalenderId = editTextCalenderId.getText().toString();
+        Spinner spinner = (Spinner)findViewById(R.id.selectThumbnail);
+        String personThumbnail = (String)spinner.getSelectedItem();
+
+        Log.v("Add Person", personName+personAddress+personDetail+personCalenderId+personThumbnail);
+
+        // display toast
+        Toast toast = Toast.makeText(
+                this,
+                R.string.finish_person_add_register,
+                Toast.LENGTH_SHORT
+        );
+        toast.show();
+
+        // transition HomeActivity
+        Intent intent = new Intent(
+               this,
+                HomeActivity.class
+        );
+        startActivity(intent);
+
         return true;
     }
 }
