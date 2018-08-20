f4373d97061e9eadd084c5c3a044b900d4060010
shishito.megane my_ubuntu <shishito.megane3060.3@gmail.com>
2018-08-20 17:13:21 +0900

    Add input complete button <- PersonAddActivity, Add icon

--- app/src/main/java/io/github/shishito_megane/dest_bbs_client_android_app/PersonAddActivity.java
+++ app/src/main/java/io/github/shishito_megane/dest_bbs_client_android_app/PersonAddActivity.java
@@ -2,6 +2,8 @@ package io.github.shishito_megane.dest_bbs_client_android_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class PersonAddActivity extends AppCompatActivity {
 
@@ -9,6 +11,20 @@ public class PersonAddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person_add);
-
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.personaddactivity_action_bar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//        if (id == R.id.home) {
+//            Intent intent = new Intent(this, NextActivity.class);
+//            intent.putExtra("hoge", "hoge");
+//            startActivity(intent);
+//        }
+        return true;
     }
 }
