1ca6348ef39d7cb50263464d8adfd6675c9bf202
shishito.megane my_ubuntu <shishito.megane3060.3@gmail.com>
2018-08-21 14:44:16 +0900

    Add check getting calender function (async) <- HomeActivity

--- app/src/main/java/io/github/shishito_megane/dest_bbs_client_android_app/HomeActivity.java
+++ app/src/main/java/io/github/shishito_megane/dest_bbs_client_android_app/HomeActivity.java
@@ -1,11 +1,19 @@
 package io.github.shishito_megane.dest_bbs_client_android_app;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.preference.PreferenceManager;
+import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +23,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +33,9 @@ import static io.github.shishito_megane.dest_bbs_client_android_app.DbContract.M
 public class HomeActivity extends AppCompatActivity {
 
     public static final String PERSON_ID = "io.github.shishito_megane.dest_bbs_client_android_app.PERSON_ID";
+
+    ProgressDialog mProgress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -115,6 +127,13 @@ public class HomeActivity extends AppCompatActivity {
         };
 
         gridview.setOnItemClickListener(mMessageClickedHandler);
+
+        // set progress bar (calender get test )
+        mProgress = new ProgressDialog(this);
+        mProgress.setMessage(getString(R.string.get_calender_toast));
+
+        // test calender
+        preCheckCalender();
 
     }
 
@@ -282,6 +301,175 @@ public class HomeActivity extends AppCompatActivity {
 
         db.close();
         mDbHelper.close();
+    }
+
+    private void preCheckCalender() {
+
+        // No Internet connection
+        if (!isDeviceOnline()) {
+            // display toast
+            Toast toast = Toast.makeText(
+                    this,
+                    R.string.no_internet_connection_msg,
+                    Toast.LENGTH_SHORT
+            );
+            toast.show();
+        }
+        else {
+            new TestTask().execute();
+        }
+    }
+
+    /**
+     * 現在、端末がネットワークに接続されているかを確認する。
+     *
+     * @return ネットワークに接続されている場合にはtrueを、そうでない場合にはfalseを返す。
+     */
+    private boolean isDeviceOnline() {
+        ConnectivityManager connMgr =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+    /**
+     * 非同期で　Google Calendar API の呼び出しを行うクラス。
+     */
+        private class TestTask extends AsyncTask<Integer, Integer, Integer> {
+
+        private Exception mLastError = null;
+
+        // プロジェクション配列。
+        // 取得したいプロパティの一覧を指定する。
+        private final String[] CALENDAR_PROJECTION = new String[] {
+                CalendarContract.Calendars._ID,
+                CalendarContract.Calendars.NAME,
+                CalendarContract.Calendars.ACCOUNT_NAME,
+                CalendarContract.Calendars.ACCOUNT_TYPE,
+                CalendarContract.Calendars.CALENDAR_COLOR,
+                CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,
+                CalendarContract.Calendars.CALENDAR_ACCESS_LEVEL,
+                CalendarContract.Calendars.CALENDAR_TIME_ZONE,
+                CalendarContract.Calendars.VISIBLE,
+                CalendarContract.Calendars.SYNC_EVENTS,
+                CalendarContract.Calendars.OWNER_ACCOUNT,
+        };
+
+        // プロジェクション配列のインデックス。
+        // パフォーマンス向上のために、動的に取得せずに、静的に定義しておく。
+        private static final int CALENDAR_PROJECTION_IDX_ID = 0;
+        private static final int CALENDAR_PROJECTION_IDX_NAME = 1;
+        private static final int CALENDAR_PROJECTION_IDX_ACCOUNT_NAME = 2;
+        private static final int CALENDAR_PROJECTION_IDX_ACCOUNT_TYPE = 3;
+        private static final int CALENDAR_PROJECTION_IDX_CALENDAR_COLOR = 4;
+        private static final int CALENDAR_PROJECTION_IDX_CALENDAR_DISPLAY_NAME = 5;
+        private static final int CALENDAR_PROJECTION_IDX_CALENDAR_ACCESS_LEVEL = 6;
+        private static final int CALENDAR_PROJECTION_IDX_CALENDAR_TIME_ZONE = 7;
+        private static final int CALENDAR_PROJECTION_IDX_VISIBLE = 8;
+        private static final int CALENDAR_PROJECTION_IDX_SYNC_EVENTS = 9;
+        private static final int CALENDAR_PROJECTION_IDX_OWNER_ACCOUNT = 10;
+
+        // 非同期処理
+        @Override
+        protected Integer doInBackground(Integer... params) {
+            int cal = getCalenderList();
+            return  cal;
+        }
+
+        private int getCalenderList()  {
+
+            // クエリ条件を設定する
+            final Uri uri = CalendarContract.Calendars.CONTENT_URI;
+            final String[] projection = CALENDAR_PROJECTION;
+            final String selection = null;
+            final String[] selectionArgs = null;
+            final String sortOrder = null;
+
+            // クエリを発行してカーソルを取得する
+            final ContentResolver cr = getContentResolver();
+            final Cursor cur = cr.query(uri, projection, selection, selectionArgs, sortOrder);
+
+            // カレンダーの総数取得
+            int calTotal = cur.getCount();
+            Log.d("カレンダー", "総数: "+String.valueOf(calTotal));
+
+            // ログ出力 (Header)
+            final StringBuilder sbHeader = new StringBuilder();
+            for (final String property : CALENDAR_PROJECTION) {
+                sbHeader.append(property).append(',');
+            }
+            Log.d("カレンダー", sbHeader.toString());
+
+            while (cur.moveToNext()) {
+                // カーソルから各プロパティを取得する
+                final long id = cur.getLong(CALENDAR_PROJECTION_IDX_ID);
+                final String name = cur.getString(CALENDAR_PROJECTION_IDX_NAME);
+                final String accountName = cur.getString(CALENDAR_PROJECTION_IDX_ACCOUNT_NAME);
+                final String accountType = cur.getString(CALENDAR_PROJECTION_IDX_ACCOUNT_TYPE);
+                final int calendarColor = cur.getInt(CALENDAR_PROJECTION_IDX_CALENDAR_COLOR);
+                final String calendarDisplayName = cur.getString(CALENDAR_PROJECTION_IDX_CALENDAR_DISPLAY_NAME);
+                final int calendarAccessLevel = cur.getInt(CALENDAR_PROJECTION_IDX_CALENDAR_ACCESS_LEVEL);
+                final String calendarTimeZone = cur.getString(CALENDAR_PROJECTION_IDX_CALENDAR_TIME_ZONE);
+                final int visible = cur.getInt(CALENDAR_PROJECTION_IDX_VISIBLE);
+                final int syncEvents = cur.getInt(CALENDAR_PROJECTION_IDX_SYNC_EVENTS);
+                final String ownerAccount = cur.getString(CALENDAR_PROJECTION_IDX_OWNER_ACCOUNT);
+
+                // ログ出力 (Body)
+                final StringBuilder sbBody = new StringBuilder();
+                sbBody.append(id).append(',');
+                sbBody.append(name).append(',');
+                sbBody.append(accountName).append(',');
+                sbBody.append(accountType).append(',');
+                sbBody.append(String.format("0x%08X", calendarColor)).append(',');
+                sbBody.append(calendarDisplayName).append(',');
+                sbBody.append(calendarAccessLevel).append(',');
+                sbBody.append(calendarTimeZone).append(',');
+                sbBody.append(visible).append(',');
+                sbBody.append(syncEvents).append(',');
+                sbBody.append(ownerAccount).append(',');
+                Log.d("カレンダー", sbBody.toString());
+            }
+
+            cur.close();
+
+            return calTotal;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            mProgress.show();
+        }
+
+        @Override
+        protected void onPostExecute(Integer intel) {
+
+            if (mProgress != null && mProgress.isShowing()) {
+                mProgress.dismiss();
+            }
+            // display toast
+            Toast toast = Toast.makeText(
+                    getApplicationContext(),
+                    getString(R.string.finish_get_calender_toast),
+                    Toast.LENGTH_SHORT
+            );
+            toast.show();
+        }
+
+        @Override
+        protected void onCancelled() {
+//            mProgress.hide();
+
+
+            // display toast
+            Toast toast = Toast.makeText(
+                    getApplicationContext(),
+                    getString(R.string.cancelled_get_calender_toast),
+                    Toast.LENGTH_SHORT
+            );
+            toast.show();
+
+        }
+
     }
 }
 
